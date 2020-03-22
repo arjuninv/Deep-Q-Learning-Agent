91b2d429548959b775b14b7583305e237e55d2e1
arjuninventor <arjun.santhoshkumar@gmail.com>
2020-03-22 21:35:09 +0530

    Agent updates

--- agent.py
+++ agent.py
@@ -1,6 +1,14 @@
+import numpy as np
+
 from envs.rover_lander_1 import rover_lander_1
 
 MAX_EPISODES = 5_000
+
+SHOW_PREVIEW = True
+AGGREGATE_STATS_EVERY = 10
+
+epsilon = 0.98
+
 
 env = rover_lander_1()
 
@@ -20,5 +28,21 @@ class Agent:
     def qs(self):
         pass
     
+agent = Agent()
+    
 for episode in tqdm(range(0, MAX_EPISODES), ascii=True, unit='episodes'):
-    pass
+    episode_reward = 0
+    step = 1
+    current_state = env.reset()
+    done = False
+    while not done:
+        if np.random.random() > epsilon:
+            action = np.argmax(agent.get_qs(current_state))
+        else:
+            action = env.random_action_sample()
+            
+    new_state, reward, done = env.step(action)
+    episode_reward += reward
+    
+    if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
+            env.render()
