f0e8540eb7b54e51da80ee4b1db7b9e1c60d9990
arjuninventor <arjun.santhoshkumar@gmail.com>
2020-03-23 22:58:19 +0530

    Added Q function, model, replay memory

--- train_worker.py
+++ train_worker.py
@@ -1,18 +1,32 @@
-import numpy as np
-import requests
-import datetime
-from collections import deque
 from envs.rover_lander_1 import rover_lander_1
+
+import random
+import datetime
+import requests
+from collections import deque
+import numpy as np
 import tqdm
 
+import tensorflow as tf
+from keras import Sequential
+from keras.layers import Dense
+from keras.optimizers import adam
+from keras.activations import relu, linear
 
 MASTER_ENDPOINT = "localhost:5000"
 WORKER_NAME = ""
 MAX_EPISODES = 5_000
 SHOW_PREVIEW = True
 AGGREGATE_STATS_EVERY = 10
+REPLAY_MEMORY_SIZE = 1000000
 
-TRAIN_PARAMS = {}
+TRAIN_PARAMS = {'learning_rate': 0.001}
+
+Q_PARAMS = {'epsilon': 1.0,
+            'gamma': 0.99,
+            'epsilon_min': 0.01,
+            'epsilon_decay': 0.996}
+
 
 id = None
 
@@ -38,44 +52,77 @@ class customCallback(tf.keras.callbacks.Callback):
         
 class Agent:
     def __init__(self):
-        self.epsilon = 1.0
-        self.epsilon_decay = .996
-        self.memory = deque(maxlen=1000000)
+        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
         self.model = self.build_model()
     
     def create_model(self):
-        pass
+        model = Sequential([Dense(150, input_dim=self.state_space, activation=relu),
+                            Dense(120, activation=relu),
+                            Dense(self.action_space, activation=linear)])
+        model.compile(loss='mse', optimizer=adam(lr=TRAIN_PARAMS['learning_rate']))
+        return model
     
     def save_model(self, local_only=True):
-        self.model.save(f"saved_model/{WORKER_NAME}.h5")
+        self.model.save(f"models/{WORKER_NAME}.h5")
         if not local_only:
-            send_model(f"saved_model/{WORKER_NAME}.h5")
+            send_model(f"models/{WORKER_NAME}.h5")
     
-    def update_replay_memory(self):
-        pass
+    def update_replay_memory(self, state, action, reward, next_state, done):
+        self.replay_memory.append((state, action, reward, next_state, done))
     
-    def train(self):
-        pass
-    
-    def qs(self):
-        pass
+    def replay(self):
+        if len(self.replay_memory) < TRAIN_PARAMS['batch_size']:
+            return
+        
+        minibatch = random.sample(self.replay_memory, TRAIN_PARAMS['batch_size'])
+        states = np.array([i[0] for i in minibatch])
+        actions = np.array([i[1] for i in minibatch])
+        rewards = np.array([i[2] for i in minibatch])
+        next_states = np.array([i[3] for i in minibatch])
+        dones = np.array([i[4] for i in minibatch])
+        
+        states = np.squeeze(states)
+        next_states = np.squeeze(next_states)
+        
+        targets = rewards + Q_PARAMS['gamma']*(np.amax(self.model.predict_on_batch(next_states), axis=1))*(1-dones)
+        targets_full = self.model.predict_on_batch(states)
+        ind = np.array([i for i in range(self.batch_size)])
+        targets_full[[ind], [actions]] = targets
+
+        self.model.fit(states, targets_full, epochs=1, verbose=0)
+        if Q_PARAMS['epsilon'] > Q_PARAMS['epsilon_min']:
+            Q_PARAMS['epsilon'] *= Q_PARAMS['epsilon_decay']
+
+
+    def qs(self, state):
+        return np.argmax(self.model.predict(np.array(state)/255))[0]
     
 env = rover_lander_1()
 agent = Agent()
-    
+
+episode_rewards = []
 for episode in tqdm(range(0, MAX_EPISODES), ascii=True, unit='episodes'):
-    # episode_reward = 0
-    # step = 1
-    # current_state = env.reset()
-    # done = False
-    # while not done:
-    #     if np.random.random() > agent.epsilon:
-    #         action = np.argmax(agent.get_qs(current_state))
-    #     else:
-    #         action = env.random_action_sample()
+    episode_reward = 0
+    step = 1
+    current_state = env.reset()
+    done = False
+    while not done:
+        if np.random.random() > agent.epsilon:
+            action = np.argmax(agent.get_qs(current_state))
+        else:
+            action = env.random_action_sample()
             
-    # new_state, reward, done = env.step(action)
-    # episode_reward += reward
+        if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
+                env.render()
+
+        new_state, reward, done = env.step(action)
+        episode_reward += reward
+        
+        agent.update_replay_memory(current_state, action, reward, new_state, done)
+        current_state = new_state
+        
+        agent.replay()
+        
     
-    # if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
-    #         env.render()
+    episode_rewards.append(episode_reward)
+    update(("last_ep_score", episode_reward), ("avg_ep_score", sum(episode_rewards) / len(episode_rewards)))
