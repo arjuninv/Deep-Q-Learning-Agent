c560daf32edce7a0f7346c082e09f9d57345128b
Prateek <emaillawrathod@gmail.com>
2020-03-24 11:07:35 +0530

    fixed

--- envs/rover_lander_1.py
+++ envs/rover_lander_1.py
@@ -1,13 +1,15 @@
-# try:
-#     from envs.core import Env
-# except:
-#     from core import Env
+try:
+    from envs.core import Env
+except:
+    from core import Env
 
 import pygame
 import math
 import random
-# from vec2d import Vec2d
+import numpy as np
 
+dt = np.dtype(int)
+dt = dt.newbyteorder('>')
 pygame.init()
 
 class rover:
@@ -31,7 +33,7 @@ class platform:
         pygame.draw.rect(self.screen, (255,255,255), self.platform, 1)
 
 
-class rover_lander_1():
+class rover_lander_1(Env):
     def __init__(self):
         self.height = 300
         self.width = 400
@@ -47,6 +49,9 @@ class rover_lander_1():
         self.rover = rover(self.screen)
         self.platform = platform(self.screen)
         self.score = 0
+        self.reward = False
+        self.surface = pygame.display.get_surface()
+
 
     def action_env(self):
         return "int ; 0:do nothing, 1:thrust down, 2: thrust left, 3:thrust right"
@@ -70,13 +75,31 @@ class rover_lander_1():
         if self.dis < 50 and self.rover.x in range(self.platform.x,self.platform.x + 30) and int(self.platform.y - self.rover.y) > 45:
             self.reset()
             self.score += 1
-            print(self.score)
+            self.reward = True
         elif self.rover.y + 50 > self.height:
             self.reset()
+
+    def random(self):
+        self.reward = False
+        self.action = random.randrange(0,4, 1)
+
+        # print(action)
+        self.platform.draw_self()
+        self.rover.draw_self()
+        self.thrust(self.action)
+        pygame.time.wait(100)
+        self.rover.y += 10
+        self.check_collision()
+        self.frame = np.frombuffer(self.surface.get_buffer(), dtype=dt)
+        pygame.display.flip()
+        self.screen.fill((0, 0, 0))
+        return (self.frame, self.reward, self.done)
+
 
     
     def user_mod(self):
         while not self.done:
+            self.reward = False
             self.platform.draw_self()
             self.rover.draw_self()
             for event in pygame.event.get():
@@ -96,11 +119,13 @@ class rover_lander_1():
 
             pygame.time.wait(10)
             self.rover.y += 1
-
-
             self.check_collision()
+            self.frame = np.frombuffer(self.surface.get_buffer(), dtype=dt)
             pygame.display.flip()
             self.screen.fill((0, 0, 0))
+            print (self.frame, self.reward, self.done)
+
+
     
     def save_frame(self):
         pass
@@ -114,12 +139,10 @@ class rover_lander_1():
     def observation(self):
         return (self.action, self.dis)
 
-    def step(self, action = None):
+    def step(self, action):
+        self.reward = False
         # while not self.done:
-        if action == None:
-            self.action = random.randrange(0,4, 1)
-        else:
-            self.action = action
+        self.action = action
         # print(action)
         self.platform.draw_self()
         self.rover.draw_self()
@@ -127,13 +150,16 @@ class rover_lander_1():
         pygame.time.wait(100)
         self.rover.y += 10
         self.check_collision()
+        self.frame = np.frombuffer(self.surface.get_buffer(), dtype=dt)
         pygame.display.flip()
-        self.screen.fill((0, 0, 0))   
+        self.screen.fill((0, 0, 0))
+        return (self.frame, self.reward, self.done)
 
 test = rover_lander_1()
 
-while True:
-    test.step()
-    print(test.observation())
+# for _ in range(3):
+#     print(test.random())
 
-# test.user_mod()
+
+
+test.user_mod()
